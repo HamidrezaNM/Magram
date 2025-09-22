ca6e8ad41d0e82e5710ecd0212f1411b7ab9e5b6
Hamidreza NM <hmdnjf86@gmail.com>
2025-09-23 03:04:34 +0330

    Fix ViteConfig

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         outDir: 'build',
         rollupOptions: {
             input: {
+                index: 'index.html',
                 sw: 'sw.js'
             }
         }
